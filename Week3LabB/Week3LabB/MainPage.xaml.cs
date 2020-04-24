ddb4507bf736ef37ffd4af713f0176bd3523c47a
GlitchyBoi69 <63305861+GlitchyBoi69@users.noreply.github.com>
2020-04-24 17:33:42 -0500

    Initial commit

---
+++ Week3LabB/Week3LabB/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Week3LabB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        void Calculate()
+        {
+            var today = DateTime.Now;
+            var bday = DatePicker.Date;
+            var age = (today - bday).TotalDays / 365;
+
+            if (age >= 21)
+            {
+                Label.Text = "You are able to have alcohol!";
+            }
+
+            else
+            {
+                Label.Text = "You cannot have alcohol...";
+            }
+        }
+    }
+}
